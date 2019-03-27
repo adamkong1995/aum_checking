863605f502295d182b0054978bd67228917b891c
adamkong1995 <adamkong256@gmail.com>
2019-03-27 15:21:09 +0800

    kelly workings on 26 Mar

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import os
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, Response
 from werkzeug.utils import secure_filename
 from helper import checking
+import time
 
 
 UPLOAD_FOLDER = os.path.abspath(os.path.dirname(__file__)) + '\\upload_folder'
@@ -21,16 +22,15 @@ def upload():
     if 'file' not in request.files:
         return "No File Uploaded1"
 
-
     file = request.files['file']
 
     if file.filename == '':
         return "No File Uploaded2"
 
     startdate = request.form['StartDate']
+
     if startdate =='':
         return "No Date selected."
-
 
     if file and allowed_file(file.filename) and startdate:
         filename = secure_filename(file.filename)
@@ -46,13 +46,14 @@ def allowed_file(filename):
 
 @app.route('/download_excel', methods=["POST"])
 def download_excel():
-     checking.download_excel()
-     return render_template('home.html')
-     # return redirect(url_for('upload'))
+    timestr = time.strftime("%Y%m%d-%H%M%S")
+    filename1 = 'result %s.xlsx' % (timestr)
+    excel = checking.download_excel()
+
+    return Response(excel, mimetype="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", headers={"Content-disposition":"attachment; filename =" + filename1})
 
 @app.errorhandler(404)
 def error_404(error):
-
     return render_template('404.html'),404
 
 
--- helper/checking.py
+++ helper/checking.py
@@ -1,7 +1,9 @@
 import pandas as pd
 from pandas.tseries.offsets import MonthEnd
-import os
 import time
+import datetime
+import dateutil.relativedelta
+from flask import Response
 
 pd.set_option('display.max_rows', 500)
 pd.set_option('display.max_columns', 500)
@@ -14,19 +16,18 @@ def Checking(filePath, startdate):
     aum = aum[['Monthend', 'Fund', 'Fund_Series', 'NAV', 'NAV_ps', 'Commitment', 'Subscription / Drawdown', 'Redemption', 'Distribution', 'Committed Capital', 'Total No. of shr','Monthly MF', 'o/s accrued PF']]
     aum = aum.rename(columns={'Monthend': 'date', 'Fund': 'fund', 'Fund_Series': 'series', 'NAV': 'nav', 'NAV_ps': 'navps', 'Commitment': 'commitment', 'Subscription / Drawdown': 'subscription', 'Redemption' :'redemption', 'Distribution': 'distribution', 'Committed Capital': 'committed', 'Total No. of shr': 'shares', 'Monthly MF': 'mf', 'o/s accrued PF': 'pf'})
     aum['series'] = aum['series'].fillna('series0')
-    aum = aum.query('date > @startdate')    # check what date
+    
+    # month minus 1
+    d = datetime.datetime.strptime(startdate, "%m-%Y")
+    d2 = d - dateutil.relativedelta.relativedelta(months=1)
 
+    aum = aum.query('date > @d2')    # check what date
 
-
-    aum['date'] = pd.to_datetime(aum['date'], format="%Y%m") + MonthEnd(0)
+    aum['date'] = pd.to_datetime(aum['date'], format="%Y%m") + MonthEnd(n=0)
     aum[['date']] = aum[['date']].astype(str)
 
     aum = aum.groupby(['date', 'fund', 'series']).sum()
     aum = aum.sort_values(by=['fund', 'series', 'date'])
-    # aum = aum.reset_index(level=0)
-    #aum[['date']] = aum[['date']].dt.date
-    #aum.dates.apply(lambda x: x.date())
-    #aum['date'] = [date.date() for date in aum['date']]
 
     aum[['nav_chg', 'navps_chg', 'commit_chg', 'sub_chg', 'redem_chg', 'dis_chg', 'commited_chg', 'shrs_chg', 'mf_chg',
          'pf_chg']] = aum.groupby(['fund', 'series'])[
@@ -38,18 +39,14 @@ def Checking(filePath, startdate):
          'redem_chg', 'distribution', 'dis_chg', 'committed', 'commited_chg', 'shares', 'shrs_chg', 'mf', 'mf_chg',
          'pf', 'pf_chg']]
 
-    aum = aum.style.applymap(check_3_pct,subset=['nav_chg', 'navps_chg', 'shrs_chg', 'mf_chg', 'pf_chg']).format({'nav':"{:.2f}",'navps':"{:.2f}"
-        ,'commitment': "{:.2f}", 'subscription': "{:.2f}", 'redemption': "{:.2f}", 'distribution': "{:.2f}", 'committed': "{:.2f}"})
+    aum = aum.style.applymap(check_3_pct,subset=['nav_chg', 'navps_chg', 'shrs_chg', 'mf_chg', 'pf_chg']).format({'nav':"{:.2f}", 'navps':"{:.2f}", 'commitment': "{:.2f}", 'subscription': "{:.2f}", 'redemption': "{:.2f}", 'distribution': "{:.2f}", 'committed': "{:.2f}"})
 
     return aum
 
 def download_excel():
-    # writer = pd.ExcelWriter('C:/Users/kellyshum/Downloads/ABCD.xlsx', engine='openpyxl')
-
     timestr = time.strftime("%Y%m%d-%H%M%S")
-    aum.to_excel('result %s.xlsx' % (timestr))
-    os.startfile('result %s.xlsx' % (timestr))
-    return aum
+    excel = aum.to_excel('result %s.xlsx' % (timestr))
+    return excel
 
 
 def check_3_pct(val):
