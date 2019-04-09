bf8abe6364f5662e9bf9bf7cc6178c022c5aa7bd
Kelly Shum <kellyshum@ASMHK.local>
2019-04-09 15:00:14 +0800

    Added checking functions

--- helper/checking.py
+++ helper/checking.py
@@ -1,10 +1,28 @@
 def to_check(val):
+    color_val = ''
     color_val = check_3_pct(val)
-    return color_val
+    color_val = check_missing(val)
+    color_val = check_no_change(val)
+    return 'background-color: %s' % color_val
 
 
 def check_3_pct(val):
     color_val = ''
     if val > 0.03 or val < -0.03:
         color_val = 'yellow'
-    return 'background-color: %s' % color_val
+    return color_val
+
+def check_missing(val):
+    missing = object()
+    if val is missing:
+        color_val = 'green'
+    return color_val
+
+def check_no_change(val):
+    if val is 0:
+        color_val = 'blue'
+    return color_val
+
+
+
+
